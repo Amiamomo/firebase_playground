2feba123f85ae69ee06db3144d590618efc6b97f
Amiamomo <alexandre.lacour@gmail.com>
2017-10-23 09:46:26 +0200

    Passage version 1.0.0

---
+++ src/config.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyDJh2S4YyvQQSiUO9JBxygOkwBL_fwM3JI",
+    authDomain: "alexzozor-ff16e.firebaseapp.com",
+    databaseURL: "https://alexzozor-ff16e.firebaseio.com",
+    projectId: "alexzozor-ff16e",
+    storageBucket: "alexzozor-ff16e.appspot.com",
+    messagingSenderId: "868939391226"
+  };
+  export default config
+  
